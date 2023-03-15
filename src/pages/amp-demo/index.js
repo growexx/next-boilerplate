0f829724ee07ee1f3d1c84d3251da0a30b5b4fea
brijesh-growexxer <brijesh.thakkar@growexx.com>
2023-03-15 18:17:33 +0530

    feat: 🎸 head tag for amp and admin page

--- src/pages/amp-demo/index.js
+++ src/pages/amp-demo/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useAmp } from 'next/amp'
+import Head from 'next/head'
 
 export const config = { amp: 'hybrid' }
 
 const AmpDemoPage = () => {
   const isAmp = useAmp()
-  return <div>{isAmp ? 'This is an AMP page' : 'This is a non-AMP page'}</div>
+  return (
+    <>
+      <Head>
+        <title>Amp page demo</title>
+      </Head>
+
+      {isAmp ? 'This is an AMP page' : 'This is a non-AMP page'}
+    </>
+  )
 }
 
 export default AmpDemoPage
